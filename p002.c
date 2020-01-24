d698c555cab432a41d8e6c12bebd24c99f9e5271
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-01-24 11:37:46 +0100

    Simplified C code of problem 2

--- p002.c
+++ p002.c
@@ -3,14 +3,13 @@
 int main(void) {
 	int former_term = 1;
 	int latter_term = 1;
-	int new_term;
 	int sum_even_valued_terms = 0;
-	do {
-		new_term = former_term + latter_term;
-		if(new_term%2 == 0) 
-			sum_even_valued_terms += new_term;	
+	while(latter_term <= 4000000) {
+		if(latter_term%2 == 0) 
+			sum_even_valued_terms += latter_term;	
+		int new_term = former_term + latter_term;
 		former_term = latter_term;
 		latter_term = new_term;
-	} while(new_term <= 4000000);
+	}; 
 	printf("%d\n", sum_even_valued_terms);
 }
