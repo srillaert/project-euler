09be2adf6a9cfc6967c0aad85aa6562d86e7260e
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-03-20 10:27:40 +0100

    Small refactorings in p027.py

--- p027.py
+++ p027.py
@@ -1,20 +1,19 @@
 from prime import is_prime
+import itertools
 
 def number_quadratic_primes(a, b):
-	n = 0
-	while True:
+	for n in itertools.count():
 		r = n*n + a*n + b
 		if not is_prime(abs(r)):
-			break
-		n += 1
-	return n
+			return n
 
+till = 1000
 max_a = 0
 max_b = 0
 max_number_primes = 0
-for b in range(-999, 1000):
+for b in range(-till+1, till):
 	if is_prime(abs(b)):
-		for a in range(-999, 1000):
+		for a in range(-till+1, till):
 			n = number_quadratic_primes(a, b)
 			if n > max_number_primes:
 				max_a = a
