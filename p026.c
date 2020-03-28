7438fc0e114c815b351a3f64cf8b94a2fa14bba7
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-03-28 20:34:13 +0100

    C solution for reciprocal cycles

---
+++ p026.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+#define TILL_D 1000
+int remainders[TILL_D]; // There cannot be more different remainders than d, the dividend
+
+int length_recurring_cycle(int d) {
+	int remainder = 1;	
+	for(int i=0; i<TILL_D; i++) {
+		remainders[i] = remainder;
+		do {
+			remainder *= 10;
+		} while(remainder < d);
+		remainder = remainder % d;
+		if(remainder == 0)
+			return 0;
+		for(int j=i; j>0; j--) {
+			if(remainders[j] == remainder)
+				return i-j+1;
+		}
+	}
+}
+
+int main() {
+	int d_longest_cycle = -1;
+	int max_length = 0;
+	for(int d=2; d<TILL_D; d++) {
+		int length = length_recurring_cycle(d);
+		if(length > max_length) {
+			max_length = length;
+			d_longest_cycle = d;
+		}
+	}
+	printf("%d\n", d_longest_cycle);
+}
