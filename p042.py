108bc2a8d20127a7aa5f90a8b4b3d03856ce2572
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-03-30 18:01:04 +0200

    Compact the Python solution for coded triangle numbers

--- p042.py
+++ p042.py
@@ -1,26 +1,18 @@
-def get_word_value(s):
-	sum = 0
-	for c in s:
-		value = ord(c) - ord('A') + 1
-		sum += value
-	return sum
+import itertools
 
-with open("p042.input", "r") as file:
-	content = file.read()
-words = content.strip('"').split('","')
+def get_word_value(word):
+	return sum(ord(c) - ord('A') + 1 for c in word)
 
 is_triangle_number = [False] * 1000
-n = 1
-triangle_number = n * (n+1) // 2
-while(triangle_number < len(is_triangle_number)):
+triangle_number = 1
+for n in itertools.count(2):
+	if (triangle_number >= len(is_triangle_number)):
+		break;
 	is_triangle_number[triangle_number] = True
-	n += 1
-	triangle_number = n * (n+1) // 2
+	triangle_number += n
 
-count = 0
-for word in words:
-	word_value = get_word_value(word)
-	if is_triangle_number[word_value]:
-		count += 1
+with open("p042.input", "r") as file:
+	words = file.read().strip('"').split('","')
 
+count = sum(1 for word in words if is_triangle_number[get_word_value(word)])
 print(count)		
