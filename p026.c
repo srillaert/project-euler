7c23650b5cc045bc0a5cd24ebb0bafd9547a14bc
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-03-30 10:43:39 +0200

    Improve speed of p026.c by indexing the array instead of searching it

--- p026.c
+++ p026.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
 
 #define TILL_D 1000
-int remainders[TILL_D]; // There cannot be more different remainders than d, the dividend
 
 int length_recurring_cycle(int d) {
-	int remainder = 1;	
-	for(int i=0; i<TILL_D; i++) {
-		remainders[i] = remainder;
+	// Initialize the array
+	int remainder_position[TILL_D]; // There are d possible different remainders : 0, 1, ..., d-1.
+	for(int i=0; i<TILL_D; i++)
+		remainder_position[i] = 0;
+
+	int remainder = 1;
+	int position = 1;
+	while(remainder_position[remainder] == 0) {
+		remainder_position[remainder] = position++;
 		do {
 			remainder *= 10;
 		} while(remainder < d);
 		remainder = remainder % d;
 		if(remainder == 0)
 			return 0;
-		for(int j=i; j>0; j--) {
-			if(remainders[j] == remainder)
-				return i-j+1;
-		}
 	}
+	return position - remainder_position[remainder] + 1;
 }
 
 int main() {
 	int d_longest_cycle = -1;
 	int max_length = 0;
-	for(int d=TILL_D-1; d>max_length /* dividend d can maximum have a cycle of length d */; d--) {
+	for(int d=TILL_D; d>max_length /* dividend d can maximum have a cycle of length d */; d--) {
 		int length = length_recurring_cycle(d);
 		if(length > max_length) {
 			max_length = length;
