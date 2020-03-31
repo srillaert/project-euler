bbc7ab49f220bf11ef5aabfa552acc848ff06d87
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-03-31 09:25:08 +0200

    Simplified p037.py

--- p037.py
+++ p037.py
@@ -1,24 +1,14 @@
-import math, prime
+import prime
 
-def remove_left_to_right(n):
-	log = int(math.log10(n))
-	for i in range(log, 0, -1):
-		yield n % 10 ** i
-
-def remove_right_to_left(n):
-	while True:
-		n //= 10
-		if n == 0:
-			break
-		yield n
+def get_truncations(n):
+	yield n
+	d = 10
+	while d <= n:
+		yield n % d
+		yield n // d
+		d *= 10
 
 till = 1000000
 is_prime = prime.get_is_prime_array(till)
-
-result = 0
-for n in range(11, till, 2):
-	if is_prime[n]:
-		if all(is_prime[m] for m in remove_left_to_right(n)):
-			if all(is_prime[m] for m in remove_right_to_left(n)):
-				result += n
+result = sum(n for n in range(11, till, 2) if all(is_prime[m] for m in get_truncations(n)))
 print(result)
