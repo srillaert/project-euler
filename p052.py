c76ba8786c257e93afe7d9011391d70ca7f73193
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-04-10 18:02:11 +0200

    Compact Python solution for permuted multiples inspired by nayuki

--- p052.py
+++ p052.py
@@ -1,22 +1,5 @@
-def equal_digits_count(a, b):
-	return all(a[i] == b[i] for i in range(10))
-
-def get_digits_count(n):
-	digits_count = [0] * 10
-	while n>0:
-		digit = n%10
-		digits_count[digit] += 1
-		n //= 10
-	return digits_count
-
-number = 0
-is_equal = False
-while not is_equal:
+# Compact solution inspired by https://github.com/nayuki/Project-Euler-solutions/blob/master/python/p052.py
+number = 1
+while not all(sorted(str(number)) == sorted(str(number * i)) for i in range(2,7)):
 	number += 1
-	digits_count_number = get_digits_count(number)
-	for multiple in range(2*number, 7*number, number):		
-		digits_count_multiple = get_digits_count(multiple)
-		is_equal = equal_digits_count(digits_count_number, digits_count_multiple)
-		if not is_equal:
-			break
 print(number)
