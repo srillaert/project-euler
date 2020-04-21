5c4a24407c9efda44260b480dcf3c94812c0cb97
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-04-21 09:30:35 +0200

    Improve execution speed fivefold by using result caching in p092.c

--- p092.c
+++ p092.c
@@ -1,4 +1,8 @@
+#include<stdbool.h>
 #include<stdio.h>
+
+#define NUMBER_LIMIT 10000000
+#define CACHE_LIMIT 9*9*7 // number with the highest square digit is 9999999
 
 int next_number(int number) {
 	int sum = 0;
@@ -12,12 +16,26 @@ int next_number(int number) {
 
 int main() {
 	int result = 0;
-	for(int i = 1; i < 10000000; i++) {
+	bool arrives_at_89[CACHE_LIMIT + 1];
+
+	for(int i = 1; i <= CACHE_LIMIT; i++) {
 		int number = i;
-		while (number != 1 && number != 89) {
+		while(1) {
+			if(number == 1) {
+				arrives_at_89[i] = false;
+				break;
+			} else if(number == 89) {
+				arrives_at_89[i] = true;
+				result++;
+				break;
+			}
 			number = next_number(number);
 		}
-		if (number == 89)
+	}
+
+	for(int i = CACHE_LIMIT + 1; i < NUMBER_LIMIT; i++) {
+		int number = next_number(i);
+		if(arrives_at_89[number])
 			result++;
 	}
 	printf("%d\n", result);
