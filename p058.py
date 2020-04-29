bb9a02199729e20dba725fad87bc9c5fdf0819ab
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-04-29 16:06:20 +0200

    Refactored p058.py

--- p058.py
+++ p058.py
@@ -1,19 +1,19 @@
+from itertools import count
 from prime import is_prime
 
-primes_count = 3
-numbers_count = 5
-right_bottom_corner = 9
-current_add = 2
-while primes_count * 100 // numbers_count >= 10:
-    current_add += 2
-    numbers_count += 4    
+primes_count = 0
+for n in count(1):
+    side_length = 2 * n + 1
+    bottom_right_corner = side_length * side_length
+    current_add = 2 * n
     extra_primes_count = sum(
         1 for n in range(
-            right_bottom_corner + current_add, 
-            right_bottom_corner + 4 * current_add + 1, 
-            current_add)
+            bottom_right_corner - 3 * current_add, 
+            bottom_right_corner, # exclude from the prime test the numbers on the bottom right diagonal, they are the odd squares so they will not be prime
+            current_add) 
             if is_prime(n))
-    right_bottom_corner += 4 * current_add
     primes_count += extra_primes_count
-side_length = current_add + 1
-print(side_length)
+    numbers_count = 1 + 4 * n
+    if primes_count * 100 // numbers_count < 10:
+        print(side_length)
+        break
