bffd8bcf1ee39d1245c37a7a9232792ea8d56a99
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-05-12 21:21:53 +0200

    Speedup p074.py twentyfold by using caching

--- p074.py
+++ p074.py
@@ -1,18 +1,23 @@
 from math import factorial
 
+factorials = [factorial(i) for i in range(10)]
 def factorial_digit_sum(n):
     result = 0
     while n > 0:
-        result += factorial(n%10)
+        result += factorials[n%10]
         n //= 10
     return result
 
+length_cache = {}
 def length_non_repeating_chain(n):
     chain = []
-    while not n in chain:
+    while not n in chain and not n in length_cache:
         chain.append(n)
         n = factorial_digit_sum(n)
-    return len(chain)
+    length = len(chain) + length_cache.get(n, 0)
+    for i in range(1, len(chain)):
+        length_cache[chain[i]] = length - i
+    return length
 
 result = sum(1 for i in range(1, 10 ** 6) if length_non_repeating_chain(i) == 60)
 print(result)
