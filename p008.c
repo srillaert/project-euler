6fbc9cf8ca0d5c88609a00b202c56698d7ebe07b
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-05-20 13:36:17 +0200

    p008.c:Reduce buffer size from 1000 to 13 bytes

--- p008.c
+++ p008.c
@@ -3,25 +3,26 @@
 #define LENGTH 13
 
 int main() {
-    FILE *fp;
-    char digits[1000];
+	FILE *fp;
+	char digits[LENGTH];
+	for(int i=0; i<LENGTH; i++)
+		digits[i] = 0;
+	int digits_insert_position = 0;
 
-    fp = fopen("p008.input", "r");
-    char c;
-    int i = 0;
-    while((c = fgetc(fp)) != EOF)
-        if(c != '\n')
-            digits[i++] = c - '0';
-    fclose(fp);
+	fp = fopen("p008.input", "r");
+	char c;
+	unsigned long long greatest_product = 0;
+	while((c = fgetc(fp)) != EOF)
+		if(c != '\n') {
+			digits[digits_insert_position] = c - '0';
+			digits_insert_position = (digits_insert_position + 1) % LENGTH;
+			unsigned long long product = 1;
+			for(int i=0; i<LENGTH; i++)
+				product *= digits[i];
+			if (product > greatest_product)
+				greatest_product = product;
+		}
+	fclose(fp);
 
-    unsigned long long greatest_product = 0;
-    for(int i = 0; i < 1000 - LENGTH; i++) {
-        unsigned long long product = 1;
-        for(int j = i; j < i + LENGTH; j++)
-            product *= digits[j];
-        if (product > greatest_product)
-            greatest_product = product;
-    }
-
-    printf("%llu\n", greatest_product);
+	printf("%llu\n", greatest_product);
 }
