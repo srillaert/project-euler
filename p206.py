f1a3d07ae6369b53c56ec78b4e31eab7b5cc391f
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-05-27 09:15:48 +0200

    p206.py:Use modulo to move the 0 check

--- p206.py
+++ p206.py
@@ -17,12 +17,10 @@ def get_candidates():
         n += 100
 
 def has_form(n):
-    if n%10 != 0:
-        return False
-    for d in range(9,0,-1):
-        n //= 100
-        if n%10 != d:
+    for d in range(10,1,-1):
+        if n%10 != d%10:
             return False
+        n //= 100
     return n == 1
 
 if __name__ == "__main__":
