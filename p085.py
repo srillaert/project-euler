49c06c399f8b0cd8677fd9178849271cd066d7b8
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2020-05-28 14:35:43 +0200

    p085.py:refactoring

--- p085.py
+++ p085.py
@@ -1,11 +1,8 @@
 from itertools import count
 from sys import maxsize
 
-def sum_numbers(till):
-    return (till * (till + 1)) // 2
-
 def rectangle_count(width, height):
-    return sum_numbers(width) * sum_numbers(height)
+    return (width * (width + 1) * height * (height + 1)) // 4
 
 def test_rectangle_count_example():
     assert rectangle_count(3, 2) == 18
@@ -18,8 +15,9 @@ if __name__ == "__main__":
     for width in count(1):
         for height in range(1, width+1):
             count = rectangle_count(width, height)
-            if abs(target - count) < closest_difference:
-                closest_difference = abs(target - count)
+            difference = abs(target - count)
+            if difference < closest_difference:
+                closest_difference = difference
                 area_closest = width * height
             if count > target + closest_difference:
                 if height == 1:
