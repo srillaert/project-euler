d4b14632a7faa0c0a9b77450adae811345def468
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2021-01-01 17:29:32 +0100

    stdint.h in p015.c

--- p015.c
+++ p015.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #define WIDTH_GRID 20
 #define EMPTY_ARRAY_ELEMENT 0 // We use 0 to indicate an empty array element
 #define SIZE_ARRAY WIDTH_GRID * 2 + 2 // We also need to leave a spot for the closing empty array element
 
-void create_next_row(unsigned long long *previous, unsigned long long *next) {
+void create_next_row(uint_fast64_t *previous, uint_fast64_t *next) {
 	next[0] = 1;
-	int i = 1;
+	size_t i = 1;
 	while(previous[i] != EMPTY_ARRAY_ELEMENT) {
 		next[i] = previous[i-1] + previous[i];
 		i++;
@@ -16,18 +17,14 @@ void create_next_row(unsigned long long *previous, unsigned long long *next) {
 }
 
 int main() {
-	unsigned long long a[SIZE_ARRAY] = { 1, 2, 1, EMPTY_ARRAY_ELEMENT };
-	unsigned long long b[SIZE_ARRAY];
+	uint_fast64_t a[SIZE_ARRAY] = { 1, 2, 1, EMPTY_ARRAY_ELEMENT };
+	uint_fast64_t b[SIZE_ARRAY];
 
-	for(int i; i < (WIDTH_GRID - 1); i++) {
+	for(uint_fast8_t i = 0; i < (WIDTH_GRID - 1); i++) {
 		create_next_row(a, b);
 		create_next_row(b, a);
 	}
 
-	//for(int i; i < SIZE_ARRAY; i++)
-	//	printf("%llu ", a[i]);
-
-	unsigned long long middle_element = a[WIDTH_GRID];
-
-	printf("%llu\n", middle_element);
+	uint_fast64_t middle_element = a[WIDTH_GRID];
+	printf("%lu\n", middle_element);
 }
