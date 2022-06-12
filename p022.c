5d674e3754c16a4e81a5e97bfc2640f9004a63c2
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2022-06-12 18:20:18 +0200

    Small improvements of p022.c

--- p022.c
+++ p022.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define BUFFER_SIZE 46447 // size of the input file, we need less
+#define BUFFER_SIZE 46447 // size of the input file, we need less because the double quotes around the strings are removed
 
 unsigned int get_alphabetical_value(char* name) {
 	unsigned int value = 0;
@@ -17,43 +17,43 @@ int pstrcmp(const void* a, const void* b) {
 }
 
 unsigned long get_total_name_scores() {
-	char buffer[BUFFER_SIZE];
+	char name_buffer[BUFFER_SIZE];
 
 	// read the file into the string buffer
 	FILE *stream = fopen("p022.input", "r");
 	char c;
 	int i = 0;
-	int word_count = 1; // there is one word more than there are commas
+	int name_count = 1; // there is one name more than there are commas
 	while((c = fgetc(stream)) != EOF) {
 		switch(c) {
 			case '"':
 				break;
 			case ',':
-				buffer[i++] = '\0';
-				word_count++;
+				name_buffer[i++] = '\0';
+				name_count++;
 				break;
 			default:
-				buffer[i++] = c;
+				name_buffer[i++] = c;
 				break;
 		}
 	}
-	buffer[i] = '\0';
+	name_buffer[i] = '\0';
 	fclose(stream);
 
 	// build an array of string pointers to the names in the buffer
-	char **names = malloc(word_count * sizeof(char*));
-	char *name = buffer - 1;
-	for(int i=0; i<word_count; i++) {
-		names[i] = name + 1;
-		while(*(++name) != '\0');	
+	char **names = malloc(name_count * sizeof(char*));
+	char *name = name_buffer;
+	for(int i=0; i<name_count; i++) {
+		names[i] = name++;
+		while(*(name++) != '\0');
 	}
 
 	// sort the names alphabetically
-	qsort(names, word_count, sizeof(char*), pstrcmp);
+	qsort(names, name_count, sizeof(char*), pstrcmp);
 
 	// finally calculate the score
 	unsigned long total_name_scores = 0;
-	for(int i=0; i<word_count; i++)
+	for(int i=0; i<name_count; i++)
 		total_name_scores += (i+1) * get_alphabetical_value(names[i]);
 
 	free(names);
