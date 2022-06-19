74ff022186342ccd8bd34bace04ff9a528a1bc03
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2022-06-19 10:44:03 +0200

    Make test.sh work with any C solution of a problem

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
-make p020
-./p020 | diff - p020.output
+#!/bin/sh
+if [ -z "$1" ]; then
+	echo "Usage : test.sh name_of_the_project_euler_problem"
+	echo "Example : test.sh p026"
+else
+	make $1
+	./$1 | diff - $1.output
+fi
