7d046c1bb17cc6e3fead30e63472290a3d4e1398
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-03-20 13:42:23 +0100

    Added p007.rs

--- p007_fast.py
+++ p007_fast.py
@@ -4,10 +4,10 @@ def get_solution():
     # for how to calculate an upper bound, see the answer on https://math.stackexchange.com/questions/54312/non-trivial-upper-bound-for-the-number-of-primes-less-or-equal-to-n
     # for n = 105000 : (n / log(n)) * (1 + 1.2762/log(n)) == 10084.14889762712, more than 10001 so this is a good upper bound
     upper_bound = 105000
-    prime_array = get_is_prime_array(upper_bound)
+    is_prime_array = get_is_prime_array(upper_bound)
     counter = 0
-    for i in range(len(prime_array)):
-        if prime_array[i]:
+    for i in range(len(is_prime_array)):
+        if is_prime_array[i]:
             counter += 1
         if counter == 10001:
             return i
