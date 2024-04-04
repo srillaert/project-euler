3701b38c333064242ce4f286303036a956a5467f
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-04-04 13:35:38 +0200

    Improve p008.c and p008.py

--- p008.c
+++ p008.c
@@ -1,29 +1,29 @@
 #include<stdio.h>
 #include<stdint.h>
 
-#define LENGTH 13
+#define NUMBER_OF_ADJACENT_DIGITS 13
 
 int main() {
 	FILE *stream;
-	uint_fast8_t digits[LENGTH];
-	for(size_t i=0; i<LENGTH; i++)
-		digits[i] = 0;
+	uint_fast8_t adjacent_digits[NUMBER_OF_ADJACENT_DIGITS];
+	for(size_t i=0; i<NUMBER_OF_ADJACENT_DIGITS; i++)
+		adjacent_digits[i] = 0;
 	size_t digits_insert_position = 0;
 
 	stream = fopen("p008.input", "r");
 	char c;
-	uint_fast64_t greatest_product = 0;
-	while((c = fgetc(stream)) != EOF)
-		if(c != '\n') {
-			digits[digits_insert_position] = c - '0';
-			digits_insert_position = (digits_insert_position + 1) % LENGTH;
-			uint_fast64_t product = 1;
-			for(size_t i=0; i<LENGTH; i++)
-				product *= digits[i];
-			if (product > greatest_product)
-				greatest_product = product;
-		}
+	uint_fast64_t greatest_digits_product = 0;
+	while((c = fgetc(stream)) != EOF) {
+		if(c == '\n') continue; // ignore the newlines between parts of the number in the input file
+		adjacent_digits[digits_insert_position] = c - '0';
+		digits_insert_position = (digits_insert_position + 1) % NUMBER_OF_ADJACENT_DIGITS;
+		uint_fast64_t digits_product = 1;
+		for(size_t i=0; i<NUMBER_OF_ADJACENT_DIGITS; i++)
+			digits_product *= adjacent_digits[i];
+		if (digits_product > greatest_digits_product)
+			greatest_digits_product = digits_product;
+	}
 	fclose(stream);
 
-	printf("%lu\n", greatest_product);
+	printf("%lu\n", greatest_digits_product);
 }
--- p008.py
+++ p008.py
@@ -1,15 +1,38 @@
 from functools import reduce # Valid in Python 2.6+, required in Python 3, from https://stackoverflow.com/questions/595374/whats-the-function-like-sum-but-for-multiplication-product
 import operator
 
-def get_greatest_product(length):
-    with open("p008.input") as input_file:
-        digits = list(int(c) for c in input_file.read().replace("\n", ""))
-    greatest_product = max(reduce(operator.mul, digits[i:i+length], 1) for i in range(1000-length))
+def get_greatest_product_list(digits, number_of_adjacent_digits):
+    greatest_product = max(
+        reduce(operator.mul, digits[i:i+number_of_adjacent_digits], 1)
+        for i in range(len(digits) - number_of_adjacent_digits + 1))
     return greatest_product
 
+def test_get_greatest_product_list_at_start():
+    digits = list(reversed(range(4)))
+    result = get_greatest_product_list(digits, 2)
+    assert result == 3 * 2
+
+def test_get_greatest_product_list_at_end():
+    digits = list(range(4))
+    result = get_greatest_product_list(digits, 2)
+    assert result == 2 * 3
+
+def get_digits_from_file():
+    with open("p008.input") as input_file:
+        digits = list(int(c) for c in input_file.read().replace("\n", ""))
+    return digits
+
+def test_get_digits_from_file():
+    digits = get_digits_from_file()
+    assert len(digits) == 1000 # problem description: file contains 1000 digit number
+
+def get_greatest_product_file(number_of_adjacent_digits):
+    digits = get_digits_from_file()
+    return get_greatest_product_list(digits, number_of_adjacent_digits)
+
 def test_example():
-    assert get_greatest_product(4) == 5832
+    assert get_greatest_product_file(4) == 5832
 
 if __name__ == "__main__":
-    greatest_product = get_greatest_product(13)
-    print(greatest_product)
+    solution = get_greatest_product_file(13)
+    print(solution)
