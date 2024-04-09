50d183e45d6b67636a6f8f93ab47d6f2fc48d31a
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-04-09 09:11:59 +0200

    Refactor p035.c

--- p035.c
+++ p035.c
@@ -1,40 +1,36 @@
-#include<math.h>
 #include<stdbool.h>
 #include<stdio.h>
 
-#define TILL 1000000
+#define EXCLUSIVE_UPPER_BOUND 1000000
+#define SQRT_EXCLUSIVE_UPPER_BOUND 1000
 
-bool is_prime[TILL];
+bool is_prime[EXCLUSIVE_UPPER_BOUND];
 
 void initialize_sieve_of_eratosthenes() {
-	for(int i=2; i<TILL; i++) 
+	for(int i=2; i<EXCLUSIVE_UPPER_BOUND; i++) 
 		is_prime[i] = true;
-	int sqrt_till = sqrt(TILL);
-	for(int i=2; i<sqrt_till; i++)
+	for(int i=2; i<SQRT_EXCLUSIVE_UPPER_BOUND; i++)
 		if(is_prime[i] == true)
-			for(int multiple=i*i; multiple<TILL; multiple+=i)
+			for(int multiple=i*i; multiple<EXCLUSIVE_UPPER_BOUND; multiple+=i)
 				is_prime[multiple] = false;
 }
 
 int main() {
 	initialize_sieve_of_eratosthenes();
 
-	int result = 1; // 2 is a circular prime but we don't include it in the loop later
-	int first_digit_factor = 1;
-	for(int n=3; n<TILL; n+=2) {
-		if(is_prime[n] == true) {
-			if(n > first_digit_factor*10) first_digit_factor *= 10;
-			int rotate = n;
-			int all_prime = true;
-			do {
-				int first_digit = rotate % 10;
-				rotate = first_digit * first_digit_factor + rotate / 10;
-				if(is_prime[rotate] == false)
-					all_prime=false;
-			} while(all_prime == true && rotate != n);
-			if(all_prime == true)
-				result++;
+	int count = 4; // 2, 3, 5 and 7 are circular primes but we start the loop later at 11
+	for(int power_of_10 = 10; power_of_10 < EXCLUSIVE_UPPER_BOUND; power_of_10 *= 10) {
+		for(int n = power_of_10 + 1; n < power_of_10 * 10; n++) {
+			if(is_prime[n] == true) {
+				int rotate = n;
+				do {
+					rotate = (rotate % 10) * power_of_10 + rotate / 10;
+				} while(rotate != n && is_prime[rotate]);
+				if(rotate == n)
+					count++;
+			}
 		}
 	}
-	printf("%d\n", result);
+
+	printf("%d\n", count);
 }
