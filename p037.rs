4b8c3a19f1caefe2d17eef6ca9a36f4a16cca7af
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-04-10 09:04:54 +0200

    Added p037.rs

---
+++ p037.rs
@@ -0,0 +1,25 @@
+mod prime;
+
+fn is_truncable_prime(is_prime: &[bool], n: usize) -> bool {
+    if !is_prime[n] { return false; }
+    let mut power_of_10 = 10;
+    while power_of_10 <= n {
+        if !is_prime[n / power_of_10] { return false; }
+        if !is_prime[n % power_of_10] { return false; }
+        power_of_10 *= 10;
+    }
+    true
+}
+
+fn main() {
+    const EXCLUDED_UPPER_BOUND: usize = 1_000_000;
+    let mut is_prime_array = [true; EXCLUDED_UPPER_BOUND];
+    prime::initialize_is_prime_slice(&mut is_prime_array);
+
+    let solution: usize = (11..EXCLUDED_UPPER_BOUND)
+        .step_by(2)
+        .filter(|&n| is_truncable_prime(&is_prime_array, n))
+        .sum();
+
+    println!("{}", solution);
+}
