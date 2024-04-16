3800ce999799b5e2371d04b9f28c12bfc6d23f99
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-04-16 15:57:47 +0200

    cache the prime list in p050.py

--- p050.py
+++ p050.py
@@ -1,38 +1,32 @@
 from itertools import takewhile
 from prime import get_is_prime_array
 
-def get_next_prime(n, is_prime_array):
-	result = n + 1
-	while not is_prime_array[result]:
-		result += 1
-	return result
+def get_primes_from_is_prime_array(is_prime_array):
+	yield 2
+	for n in range(3, len(is_prime_array), 2):
+		if is_prime_array[n]:
+			yield n
 
-def get_consecutive_primes(prime, is_prime_array):
-	while True:
-		yield prime
-		prime = get_next_prime(prime, is_prime_array)
-
-def aggregate_consecutive_primes(start_prime, is_prime_array, till_sum):
+def aggregate_consecutive_primes(primes, i):
 	sum = 0
 	count = 0
-	for prime in get_consecutive_primes(start_prime, is_prime_array):
-		sum += prime
+	for j in range(i, len(primes)):
+		sum += primes[j]
 		count += 1
-		if sum >= till_sum:
-			break
 		yield count, sum
 
 # Find the biggest prime less than 1 million that is the sum of the most consecutive primes
 def get_solution(till_sum):
 	is_prime_array = get_is_prime_array(till_sum)
+	primes = list(get_primes_from_is_prime_array(is_prime_array))
 	# the prime 5 is the sum of the 2 consecutive primes 2 and 3
 	max_count = 2
 	prime_sum = 5
-	for start_prime in takewhile(lambda p: p <= (till_sum // max_count), get_consecutive_primes(2, is_prime_array)):
-		for count, sum in aggregate_consecutive_primes(start_prime, is_prime_array, till_sum):
+	for i in takewhile(lambda i: primes[i] <= till_sum // max_count, range(len(primes))):
+		for count, sum in takewhile(lambda t: t[1] <= till_sum, aggregate_consecutive_primes(primes, i)):
 			if count > max_count and is_prime_array[sum]:
 				max_count = count
-				prime_sum = sum			
+				prime_sum = sum
 	return prime_sum
 
 if __name__ == "__main__":
--- p050_test.py
+++ p050_test.py
@@ -1,17 +1,10 @@
-from itertools import islice
-from p050 import get_consecutive_primes, get_next_prime, get_solution
+from p050 import get_primes_from_is_prime_array, get_solution
 from prime import get_is_prime_array
 
-def test_get_next_prime():
-    is_prime_array = get_is_prime_array(20)
-    assert(get_next_prime(2, is_prime_array) == 3)
-    assert(get_next_prime(3, is_prime_array) == 5)
-    assert(get_next_prime(7, is_prime_array) == 11)
-
-def test_get_consecutive_primes():
+def test_get_primes_from_is_prime_array():
     is_prime_array = get_is_prime_array(10)
-    actual = list(islice(get_consecutive_primes(2, is_prime_array), 3))
-    assert(actual == [2, 3, 5])
+    actual = list(get_primes_from_is_prime_array(is_prime_array))
+    assert(actual == [2, 3, 5, 7])
 
 def test_get_solution_examples():
     assert(get_solution(100) == 41)
