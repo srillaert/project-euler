5c980dee1dac25fa772cdd6c7ea955daa6863d56
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-04-19 11:51:46 +0200

    PrimeSieve : Only store the odd numbers

--- prime_sieve.rs
+++ prime_sieve.rs
@@ -1,17 +1,19 @@
+use std::iter;
+
 pub struct PrimeSieve {
     is_prime_slice: Vec<bool>
 }
 
 impl PrimeSieve {
     pub fn new(exclusive_upper_bound: usize) -> Self {
-        let mut is_prime = vec![true; exclusive_upper_bound];
+        let vec_len = exclusive_upper_bound / 2;
+        let mut is_prime = vec![true; vec_len];
         is_prime[0] = false;
-        is_prime[1] = false;
         let square_root = ((exclusive_upper_bound - 1) as f64).sqrt() as usize;
-        for i in 0..=square_root {
-            if is_prime[i] {
-                for j in (i*i..exclusive_upper_bound).step_by(i) {
-                    is_prime[j] = false;
+        for i in (3..=square_root).step_by(2) {            
+            if is_prime[i/2] {                
+                for j in (i*i..exclusive_upper_bound).step_by(2*i) {
+                    is_prime[j/2] = false;
                 }
             }
         }
@@ -21,20 +23,25 @@ impl PrimeSieve {
     }
 
     pub fn get_primes<'a>(&'a self) -> impl Iterator<Item = usize> + 'a {
-        (2..self.is_prime_slice.len()).filter(move |&n| self.is_prime(n))
+        let odd_primes_iterator = self.is_prime_slice.iter().enumerate()
+            .filter_map(|(index, is_prime)| if *is_prime { Some(index * 2 + 1) } else { None });
+        iter::once(2).chain(odd_primes_iterator)
     }
 
     pub fn is_prime(&self, n: usize) -> bool {
-        self.is_prime_slice[n]
+        if n%2 == 0 {
+            return n == 2;
+        }
+        self.is_prime_slice[n/2]
     }
 }
 
 #[test]
-fn test_is_prime() {
+fn test_new() {
     let sieve = PrimeSieve::new(10);
 
-    assert_eq!(sieve.is_prime(0), false);
-    assert_eq!(sieve.is_prime(2), true);
+    let expected = vec![false, true, true, true, false];
+    assert_eq!(sieve.is_prime_slice, expected);
 }
 
 #[test]
@@ -45,4 +52,14 @@ fn test_get_primes() {
 
     let expected = vec![2, 3, 5, 7, 11, 13, 17, 19];
     assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_is_prime() {
+    let sieve = PrimeSieve::new(10);
+
+    assert_eq!(sieve.is_prime(0), false);
+    assert_eq!(sieve.is_prime(2), true);
+    assert_eq!(sieve.is_prime(3), true);
+    assert_eq!(sieve.is_prime(4), false);
 }
