bdcd9726e77945ab76750b8fc90a1d05d28f23aa
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-04-19 09:22:33 +0200

    Use mod prime_sieve in p050.rs

--- p050.rs
+++ p050.rs
@@ -1,4 +1,4 @@
-mod prime;
+mod prime_sieve;
 
 fn aggregate_consecutive_primes(primes: &[usize], i: usize) -> impl Iterator<Item = (usize, usize)> + '_ {
     let mut sum = 0;
@@ -11,20 +11,20 @@ fn aggregate_consecutive_primes(primes: &[usize], i: usize) -> impl Iterator<Ite
 }
 
 // Find the biggest prime less than is_prime_array.len() that is the sum of the most consecutive primes
-pub fn get_solution(is_prime_array: &mut [bool]) -> usize {
-    prime::prime_sieve_initialize(is_prime_array);
-    let primes = prime::prime_sieve_get_primes(& is_prime_array).collect::<Vec<_>>();
+pub fn get_solution(exclusive_upper_bound: usize) -> usize {
+    let sieve = prime_sieve::PrimeSieve::new(exclusive_upper_bound);
+    let primes = sieve.get_primes().collect::<Vec<_>>();
 
     // the prime 5 is the sum of the 2 consecutive primes 2 and 3
     let mut max_count = 2;
     let mut prime_sum = 5;
 
     for i in 0..primes.len() {
-        if primes[i] >= is_prime_array.len() / max_count {
+        if primes[i] >= exclusive_upper_bound / max_count {
             break;
         }
-        for (count, sum) in aggregate_consecutive_primes(&primes, i).take_while(|&(_, sum)| sum < is_prime_array.len()) {
-            if count > max_count && is_prime_array[sum] {
+        for (count, sum) in aggregate_consecutive_primes(&primes, i).take_while(|&(_, sum)| sum < exclusive_upper_bound) {
+            if count > max_count && sieve.is_prime(sum) {
                 max_count = count;
                 prime_sum = sum;
             }
@@ -35,15 +35,11 @@ pub fn get_solution(is_prime_array: &mut [bool]) -> usize {
 
 #[test]
 fn test_get_solution_examples() {
-    let mut hundred_array = [true; 100];
-    assert_eq!(get_solution(&mut hundred_array), 41);
-
-    let mut thousand_array = [true; 1000];
-    assert_eq!(get_solution(&mut thousand_array), 953)
+    assert_eq!(get_solution(100), 41);
+    assert_eq!(get_solution(1000), 953)
 }
 
 fn main() {
-    let mut is_prime_array = [true; 1_000_000];
-    let prime_sum = get_solution(&mut is_prime_array);
+    let prime_sum = get_solution(1_000_000);
     println!("{}", prime_sum);
 }
