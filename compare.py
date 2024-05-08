79444d1816b4ade97c3ef1411b1f28efcbc71dca
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-05-08 09:14:45 +0200

    Refactored compare.py

--- compare.py
+++ compare.py
@@ -4,39 +4,40 @@ import subprocess
 import sys
 import time
 
+def run_solver(run_args, file_name):
+    start_time = time.time()
+    result = subprocess.run(run_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    elapsed_time = time.time() - start_time
+    return (file_name, result.stdout.strip(), elapsed_time)
+
+def run_make_and_solver(executable, file_name):
+    result = subprocess.run(["/usr/bin/make", executable], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if result.returncode == 0:
+        return run_solver(["./" + executable], file_name)
+    else:
+        print(f"Error building:")
+        print(result.stderr)
+        return
+
+def get_output(file_name):
+    path = Path(file_name)
+    if path.suffix == ".py" and not path.stem.endswith("_test"):
+        return run_solver(["python3", file_name], file_name)
+    elif path.suffix == ".rs":
+        executable = path.stem + "r"
+        return run_make_and_solver(executable, file_name)
+    elif path.suffix == ".c" and not path.stem.endswith("_lib") and not path.stem.endswith("_test"):
+        executable = path.stem + "c"
+        return run_make_and_solver(executable, file_name)
+    else:
+        return
+
 def get_outputs(problem):
     matching_files = glob.glob(problem + '*.*')
     for file_name in matching_files:
-        path = Path(file_name)
-        if path.suffix == ".py" and not path.stem.endswith("_test"):
-            start_time = time.time()
-            result = subprocess.run(["python3", file_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            elapsed_time = time.time() - start_time
-            yield (file_name, result.stdout.strip(), elapsed_time)
-        elif path.suffix == ".rs":
-            executable = path.stem + "r"
-            result = subprocess.run(["/usr/bin/make", executable], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            if result.returncode == 0:
-                start_time = time.time()
-                result = subprocess.run(["./" + executable], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                elapsed_time = time.time() - start_time
-                yield (file_name, result.stdout.strip(), elapsed_time)
-            else:
-                print(f"Error building:")
-                print(result.stderr)
-        elif path.suffix == ".c" and not path.stem.endswith("_test"):
-            executable = path.stem + "c"
-            result = subprocess.run(["/usr/bin/make", executable], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            if result.returncode == 0:
-                start_time = time.time()
-                result = subprocess.run(["./" + executable], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                elapsed_time = time.time() - start_time
-                yield (file_name, result.stdout.strip(), elapsed_time)
-            else:
-                print(f"Error building:")
-                print(result.stderr)
-        else:
-            continue
+        result = get_output(file_name)
+        if result != None:
+            yield result        
 
 def print_table(problem):
     print(f'{"program".ljust(15)} output\telapsed_seconds')
