853a12c6aac34ede2b09cd751a42a5044f7c308c
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-05-27 10:46:54 +0200

    Use BitVec instead of Vec<bool> in prime_sieve.rs

--- prime_sieve.rs
+++ prime_sieve.rs
@@ -1,30 +1,31 @@
+use bitvec::prelude::*;
 use std::iter;
 
 pub struct PrimeSieve {
-    is_prime_slice: Vec<bool>
+    is_prime_vector: BitVec
 }
 
 impl PrimeSieve {
     pub fn new(exclusive_upper_bound: usize) -> Self {
         let vec_len = exclusive_upper_bound / 2;
-        let mut is_prime = vec![true; vec_len];
-        is_prime[0] = false;
+        let mut is_prime = bitvec![1; vec_len];
+        is_prime.set(0, false);
         let square_root = ((exclusive_upper_bound - 1) as f64).sqrt() as usize;
-        for i in (3..=square_root).step_by(2) {            
-            if is_prime[i/2] {                
+        for i in (3..=square_root).step_by(2) {
+            if is_prime[i/2] {
                 for j in (i*i..exclusive_upper_bound).step_by(2*i) {
-                    is_prime[j/2] = false;
+                    is_prime.set(j/2, false);
                 }
             }
         }
         Self {
-            is_prime_slice: is_prime
+            is_prime_vector: is_prime
         }
     }
 
     #[allow(dead_code)]
     pub fn get_primes<'a>(&'a self) -> impl Iterator<Item = usize> + 'a {
-        let odd_primes_iterator = self.is_prime_slice.iter().enumerate()
+        let odd_primes_iterator = self.is_prime_vector.iter().enumerate()
             .filter_map(|(index, is_prime)| if *is_prime { Some(index * 2 + 1) } else { None });
         iter::once(2).chain(odd_primes_iterator)
     }
@@ -34,16 +35,19 @@ impl PrimeSieve {
         if n%2 == 0 {
             return n == 2;
         }
-        self.is_prime_slice[n/2]
+        self.is_prime_vector[n/2]
     }
 }
 
 #[test]
 fn test_new() {
-    let sieve = PrimeSieve::new(10);
+	let sieve = PrimeSieve::new(10);
 
-    let expected = vec![false, true, true, true, false];
-    assert_eq!(sieve.is_prime_slice, expected);
+	assert!(!sieve.is_prime_vector[0], "1 is not prime");
+	assert!(sieve.is_prime_vector[1], "3 is prime");
+	assert!(sieve.is_prime_vector[2], "5 is prime");
+	assert!(sieve.is_prime_vector[3], "7 is prime");
+	assert!(!sieve.is_prime_vector[4], "9 is not prime");
 }
 
 #[test]
