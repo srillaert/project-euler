12fa02f8c91db078ffc8e1a93edd1ec6456b2f54
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2024-06-05 21:22:07 +0200

    Replaced bitvec crate by custom BitVector

--- prime_sieve.rs
+++ prime_sieve.rs
@@ -1,20 +1,99 @@
-use bitvec::prelude::*;
 use std::iter;
 
+pub struct BitVector {
+	exclusive_upper_bound: usize,
+    vector: Vec<usize>
+}
+
+impl BitVector {
+    pub fn new(exclusive_upper_bound: usize) -> Self {
+		let mut vector_len = exclusive_upper_bound / (usize::BITS as usize);
+		if exclusive_upper_bound % (usize::BITS as usize) > 0 {
+			vector_len += 1;
+		}
+		Self {
+			exclusive_upper_bound: exclusive_upper_bound,
+            vector: vec![usize::MAX; vector_len]
+        }
+    }
+
+    pub fn get_true_indices<'a>(&'a self) -> impl Iterator<Item = usize> + 'a {
+		let iterator = (0..self.exclusive_upper_bound)
+			.filter_map(move |i| if self.get_value(i) { Some(i) } else { None });
+		iterator
+    }
+
+	pub fn get_value(&self, nth: usize) -> bool {
+		let index = nth / (usize::BITS as usize);
+		let element = self.vector[index];
+		let n = nth % (usize::BITS as usize);
+		((element >> n) & 1) == 1
+	}
+
+	pub fn set_value(&mut self, nth: usize, value: bool) {
+		let index = nth / (usize::BITS as usize);
+		let n = nth % (usize::BITS as usize);
+		let element = if value { self.vector[index] | (1 << n) } else { self.vector[index] & !(1 << n) };
+		self.vector[index] = element;
+	}
+}
+
+#[test]
+fn test_new() {
+	assert_eq!(BitVector::new(0).vector.len(), 0);
+	assert_eq!(BitVector::new(1).vector.len(), 1);
+	assert_eq!(BitVector::new(usize::BITS as usize).vector.len(), 1);
+	assert_eq!(BitVector::new((usize::BITS as usize) + 1).vector.len(), 2);
+}
+
+#[test]
+fn test_get_true_indices() {
+	let mut bit_vector = BitVector::new(3);
+	bit_vector.set_value(1, false);
+
+	let actual: Vec<_> = bit_vector.get_true_indices().collect();
+
+	let expected = vec![0usize, 2usize];
+	assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_get_value() {
+	let bit_vector = BitVector::new(1);
+	assert!(bit_vector.get_value(0));
+	assert!(bit_vector.get_value((usize::BITS as usize) - 1));
+}
+
+#[test]
+fn test_set_value() {
+	let mut bit_vector = BitVector::new(1);
+	assert_eq!(bit_vector.vector[0].count_ones(), usize::BITS);
+
+	bit_vector.set_value(0, false);
+
+	assert!(!bit_vector.get_value(0));
+	assert_eq!(bit_vector.vector[0].count_ones(), usize::BITS - 1);
+
+	bit_vector.set_value(0, true);
+
+	assert!(bit_vector.get_value(0));
+	assert_eq!(bit_vector.vector[0].count_ones(), usize::BITS);
+}
+
 pub struct PrimeSieve {
-    is_prime_vector: BitVec
+    is_prime_vector: BitVector
 }
 
 impl PrimeSieve {
     pub fn new(exclusive_upper_bound: usize) -> Self {
         let vec_len = exclusive_upper_bound / 2;
-        let mut is_prime = bitvec![1; vec_len];
-        is_prime.set(0, false);
+        let mut is_prime = BitVector::new(vec_len);
+        is_prime.set_value(0, false);
         let square_root = ((exclusive_upper_bound - 1) as f64).sqrt() as usize;
         for i in (3..=square_root).step_by(2) {
-            if is_prime[i/2] {
+            if is_prime.get_value(i/2) {
                 for j in (i*i..exclusive_upper_bound).step_by(2*i) {
-                    is_prime.set(j/2, false);
+                    is_prime.set_value(j/2, false);
                 }
             }
         }
@@ -25,8 +104,7 @@ impl PrimeSieve {
 
     #[allow(dead_code)]
     pub fn get_primes<'a>(&'a self) -> impl Iterator<Item = usize> + 'a {
-        let odd_primes_iterator = self.is_prime_vector.iter().enumerate()
-            .filter_map(|(index, is_prime)| if *is_prime { Some(index * 2 + 1) } else { None });
+		let odd_primes_iterator = self.is_prime_vector.get_true_indices().map(|index| index * 2 + 1);
         iter::once(2).chain(odd_primes_iterator)
     }
 
@@ -35,19 +113,19 @@ impl PrimeSieve {
         if n%2 == 0 {
             return n == 2;
         }
-        self.is_prime_vector[n/2]
+        self.is_prime_vector.get_value(n/2)
     }
 }
 
 #[test]
-fn test_new() {
+fn test_prime_sieve_new() {
 	let sieve = PrimeSieve::new(10);
 
-	assert!(!sieve.is_prime_vector[0], "1 is not prime");
-	assert!(sieve.is_prime_vector[1], "3 is prime");
-	assert!(sieve.is_prime_vector[2], "5 is prime");
-	assert!(sieve.is_prime_vector[3], "7 is prime");
-	assert!(!sieve.is_prime_vector[4], "9 is not prime");
+	assert!(!sieve.is_prime_vector.get_value(0), "1 is not prime");
+	assert!(sieve.is_prime_vector.get_value(1), "3 is prime");
+	assert!(sieve.is_prime_vector.get_value(2), "5 is prime");
+	assert!(sieve.is_prime_vector.get_value(3), "7 is prime");
+	assert!(!sieve.is_prime_vector.get_value(4), "9 is not prime");
 }
 
 #[test]
