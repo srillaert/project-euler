4dc6923c64843fff718138a200660067b684caf6
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2025-01-11 10:42:34 +0100

    p800.py : Improve test run time by using smaller sieves.

--- p800.py
+++ p800.py
@@ -1,22 +1,23 @@
 from prime_sieve import PrimeSieve
-from math import log
+from math import log, log2
 
 def get_number_hybrid_integers(base, exponent):
 	log_till = exponent * log(base)
-	sieve_till = 16000000 # 800800 * log2(800800)
+	sieve_till = int(exponent * log2(base))
 	sieve = PrimeSieve(sieve_till)
 	primes = list(sieve.get_primes())
 	count = 0
 	for i in range(len(primes)):
 		p = primes[i]
-		if 2 * p * log(p) >= log_till:
+		log_p = log(p)
+		if 2 * p * log_p >= log_till:
 			break
 		# Use binary search to find the first prime q where p**q * q**p > base**exponent
 		low, high = i + 1, len(primes)
 		while low < high:
 			mid = (low + high) // 2
 			q = primes[mid]
-			log_hybrid_integer = q * log(p) + p * log(q)
+			log_hybrid_integer = q * log_p + p * log(q)
 			if log_hybrid_integer > log_till + 1e-9:
 				high = mid
 			else:
