b109b5003ad6da02d9410b14f4e011f50f7aea97
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2025-01-17 22:48:35 +0100

    Make p500_test.py faster

--- p500.py
+++ p500.py
@@ -1,8 +1,8 @@
 from functools import reduce
 from prime_sieve import PrimeSieve
 
-def get_factors(exponent_of_2):
-	sieve = PrimeSieve(10000000)
+def get_factors(primes_till, exponent_of_2):
+	sieve = PrimeSieve(primes_till)
 	primes = list(sieve.get_primes())
 	indexes = [0]
 	for _ in range(exponent_of_2):
@@ -13,10 +13,10 @@ def get_factors(exponent_of_2):
 			indexes.append(0)
 		indexes[min_index] = indexes[min_index] + 1
 
-def get_smallest_numbers_with_power_of_2_divisors(exponent_of_2):
-	result = reduce(lambda acc, num: (acc * num) % 500500507, get_factors(exponent_of_2), 1)
+def get_smallest_number_with_power_of_2_divisors(primes_till, exponent_of_2):
+	result = reduce(lambda acc, num: (acc * num) % 500_500_507, get_factors(primes_till, exponent_of_2), 1)
 	return result
 
 if __name__ == "__main__":
-	solution = get_smallest_numbers_with_power_of_2_divisors(500500)
+	solution = get_smallest_number_with_power_of_2_divisors(8_000_000, 500_500)
 	print(solution)
--- p500_test.py
+++ p500_test.py
@@ -1,10 +1,10 @@
 from p500 import *
 
 def test_get_factors():
-	assert list(get_factors(7)) == [2, 3, 4, 5, 7, 9, 11]
+	assert list(get_factors(100, 7)) == [2, 3, 4, 5, 7, 9, 11]
 
-def test_get_smallest_numbers_with_power_of_2_divisors():
-	assert get_smallest_numbers_with_power_of_2_divisors(3) == 24
-	assert get_smallest_numbers_with_power_of_2_divisors(4) == 120
-	assert get_smallest_numbers_with_power_of_2_divisors(5) == 840
-	assert get_smallest_numbers_with_power_of_2_divisors(6) == 7560
+def test_get_smallest_number_with_power_of_2_divisors():
+	assert get_smallest_number_with_power_of_2_divisors(100, 3) == 24
+	assert get_smallest_number_with_power_of_2_divisors(100, 4) == 120
+	assert get_smallest_number_with_power_of_2_divisors(100, 5) == 840
+	assert get_smallest_number_with_power_of_2_divisors(100, 6) == 7560
