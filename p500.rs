8da609b6ed965753a1cbf69af90c3783b91c37e7
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2025-04-15 15:54:38 +0200

    Improved p500 .md and .rs

--- p500.rs
+++ p500.rs
@@ -7,7 +7,7 @@ fn get_factors_doubling_divisors(sieve_till: usize, number_factors: usize) -> im
 	let primes= sieve.get_primes().take(number_factors).collect::<Vec<_>>();
 	let largest_prime = primes[primes.len() - 1];
 	let sqrt_largest_prime = largest_prime.integer_sqrt();
-	let squared_primes = primes
+	let primes_power_2n = primes
 		.iter()
 		.take_while(|&p| *p <= sqrt_largest_prime)
 		.flat_map(|p| {
@@ -22,13 +22,13 @@ fn get_factors_doubling_divisors(sieve_till: usize, number_factors: usize) -> im
 			})
 		})
 		.sorted();
-	let factors = primes.into_iter().merge(squared_primes).take(number_factors);
+	let factors = primes.into_iter().merge(primes_power_2n).take(number_factors);
 	factors
 }
 
 #[test]
 fn test_get_factors_doubling_divisors() {
-	assert_eq!(get_factors_doubling_divisors(100, 7).collect::<Vec<_>>(), [2, 3, 4, 5, 7, 9, 11]);
+	assert_eq!(get_factors_doubling_divisors(100, 9).collect::<Vec<_>>(), [2, 3, 4, 5, 7, 9, 11, 13, 16]);
 }
 
 fn get_smallest_number_with_power_of_2_divisors(sieve_till: usize, number_factors: usize) -> usize {
