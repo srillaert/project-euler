c936f388ce0742e68926de6dad0566589d293149
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2025-04-17 14:36:56 +0200

    Improve Python solution of p081

--- p081.py
+++ p081.py
@@ -1,19 +1,21 @@
-import sys
+# Based on https://blog.dreamshire.com/project-euler-81-solution/ and https://www.mathblog.dk/project-euler-81-find-the-minimal-path-sum-from-the-top-left-to-the-bottom-right-by-moving-right-and-down/
+# This solution is easier to understand than my own original solution (see first commit of p081.py)
+def get_minimal_path_sum(filename):
+    with open(filename) as f:
+        matrix = [list(map(int, line.split(','))) for line in f.readlines()]
 
-matrix = [[int(number) for number in line.split(',')] for line in open('p081.input')]
+    # Modify the matrix in-place, where each cell eventually contains the minimum path sum to reach that cell
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            # For each position (i,j), add to its current value the minimum of the value above or the value to the left
+            matrix[i][j] += \
+                min(matrix[i-1][j], matrix[i][j-1]) if i * j > 0 else \
+                matrix[i-1][j] if i else \
+                matrix[i][j-1] if j else \
+                0
 
-def get_matrix_diagonal_item(diagonal_index, item_index):
-    matrix_row = diagonal_index - item_index
-    if matrix_row < 0 or matrix_row >= len(matrix):
-        return sys.maxsize
-    else:
-        return matrix[matrix_row][item_index]
+    return matrix[-1][-1]
 
-diagonal = [sys.maxsize] * len(matrix)
-diagonal[0] = matrix[0][0]
-for diagonal_index in range(1, 2 * len(matrix) - 1):
-    for item_index in reversed(range(1, len(matrix))):
-        diagonal[item_index] = get_matrix_diagonal_item(diagonal_index, item_index) + min(diagonal[item_index], diagonal[item_index - 1])
-    diagonal[0] = get_matrix_diagonal_item(diagonal_index, 0) + diagonal[0]
-
-print(diagonal[len(matrix) - 1])
+if __name__ == "__main__":
+    solution = get_minimal_path_sum("p081.input")
+    print(solution)
---
+++ p081_test.py
@@ -0,0 +1,5 @@
+from p081 import *
+
+def test_get_minimal_path_sum_example():
+	actual = get_minimal_path_sum("p081_example.input")
+	assert actual == 2427
