b8f728df0601fd500e6d3a32fe89473bdc8b0fdb
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2025-04-17 04:20:54 +0200

    Improved p082.py

--- p082.py
+++ p082.py
@@ -1,25 +1,21 @@
-def get_column(matrix, i):
-    return [row[i] for row in matrix]
-
 def add_vectors(vector_a, vector_b):
-    return [sum(i) for i in zip(vector_a, vector_b)]
+    return [a + b for a, b in zip(vector_a, vector_b)]
 
-def minimal_path_sum(matrix):
-    n = len(matrix)
-    minimal = get_column(matrix, n-1)
-    for i in range(n-2, 0, -1): # iterate over the middle columns from right to left
-        column = get_column(matrix, i)
+def minimal_path_sum(rows):
+    columns = list(map(list, zip(*rows))) # transpose rows to columns
+    n = len(columns)
+    minimal = columns[0]
+    for column in columns[1:-1]:
         minimal = add_vectors(minimal, column)
-        for step in range(n-1):
-            for j in range(1, n): # steps up
-                minimal[j] = min(minimal[j], column[j] + minimal[j-1])
-            for j in range(n-2, -1, -1): # steps down
-                minimal[j] = min(minimal[j], column[j] + minimal[j+1])
-    first_column = get_column(matrix, 0)
-    minimal = add_vectors(minimal, first_column)    
+        for j in range(1, n): # move up
+            minimal[j] = min(minimal[j], column[j] + minimal[j-1])
+        for j in reversed(range(n - 1)): # move down
+            minimal[j] = min(minimal[j], column[j] + minimal[j+1])
+    minimal = add_vectors(minimal, columns[-1])
     result = min(minimal)
     return result
 
 if __name__ == "__main__":
-    matrix = [[int(number) for number in line.split(',')] for line in open('p082.input')]
-    print(minimal_path_sum(matrix))
+    with open('p082.input') as f:
+        rows = [list(map(int, line.strip().split(','))) for line in f]
+    print(minimal_path_sum(rows))
