20503d175257fc92db8835c66949baf385f6ad8e
Stefaan Rillaert <stefaan.rillaert@gmail.com>
2025-05-06 08:59:34 +0200

    p061.py : Improved code

--- p061.py
+++ p061.py
@@ -1,47 +1,56 @@
+# generator formulae
+def p3(n): return n * (n + 1) // 2
+def p4(n): return n * n
+def p5(n): return n * (3 * n - 1) // 2
+def p6(n): return n * (2 * n - 1)
+def p7(n): return n * (5 * n - 3) // 2
+def p8(n): return n * (3 * n - 2)
+
 def get_figurate_numbers_helper(generator):
 	n = 1
 	while True:
-		square = generator(n)
-		if square >= 10000:
+		figurate_number = generator(n)
+		if figurate_number >= 10000:
 			break
-		if square >= 1000:
-			yield square
+		if figurate_number >= 1000:
+			yield figurate_number
 		n += 1
 
 def get_figurate_numbers(generators):
-	return list((n, i) for i, g in enumerate(generators) for n in get_figurate_numbers_helper(g))
+	return [(n, i) for i, g in enumerate(generators) for n in get_figurate_numbers_helper(g)]
 
 def get_cyclical_figurate_numbers(lookup, current, stop, visited_types):
 	lookup_items = lookup.get(current%100)
 	for lookup_item in lookup_items:
-		n, pol_type = lookup_item
+		n, figurate_type = lookup_item
 		if n == stop:
 			if all(visited_types):
 				return [current]
 			else:
 				return None
-		elif visited_types[pol_type]:
-			continue
+		elif visited_types[figurate_type]:
+			continue # we have already visited this type without reaching a cycle
 		else:
-			visited_types[pol_type] = True
+			visited_types[figurate_type] = True
 			result = get_cyclical_figurate_numbers(lookup, n, stop, visited_types)
 			if result != None:
 				result.append(current)
 				return result
-			visited_types[pol_type] = False
+			visited_types[figurate_type] = False
 	return None
 
-def get_cyclical_figurate_numbers_from_list(figurative_numbers, types_count):
+def get_cyclical_figurate_numbers_from_list(figurate_numbers):
+	types_count = max(figurate_type for _, figurate_type in figurate_numbers) + 1
 	lookup = dict()
 	for n in range(100):
 		lookup[n] = []
-	for n, pol_type in figurative_numbers:
-		lookup[n//100].append((n, pol_type))
-	
+	for n, figurate_type in figurate_numbers:
+		lookup[n//100].append((n, figurate_type))
+
 	for lookup_value in lookup.values():
-		for start, pol_type in lookup_value:
+		for start, figurate_type in lookup_value:
 			visited_types = [False] * types_count
-			visited_types[pol_type] = True
+			visited_types[figurate_type] = True
 			result = get_cyclical_figurate_numbers(lookup, start, start, visited_types)
 			if result != None:
 				result.reverse()
@@ -49,15 +58,8 @@ def get_cyclical_figurate_numbers_from_list(figurative_numbers, types_count):
 	return result
 
 if __name__ == "__main__":
-	generators = [
-		lambda n : n * (n + 1) // 2,
-		lambda n : n * n,
-		lambda n : n * (3 * n - 1) // 2,
-		lambda n : n * (2 * n - 1),
-		lambda n : n * (5 * n - 3) // 2,
-		lambda n : n * (3 * n - 2),
-	]
+	generators = [ p3, p4, p5, p6, p7, p8 ]
 	figurate_numbers = get_figurate_numbers(generators)
-	cyclical_figurate_numbers = get_cyclical_figurate_numbers_from_list(figurate_numbers, 6)
+	cyclical_figurate_numbers = get_cyclical_figurate_numbers_from_list(figurate_numbers)
 	solution = sum(cyclical_figurate_numbers)
 	print(solution)
--- p061_test.py
+++ p061_test.py
@@ -1,40 +1,32 @@
 from p061 import *
 
 def test_get_figurate_numbers():
-	generators = [
-		lambda n : n * (n + 1) // 2,
-		lambda n : n * n,
-		lambda n : n * (3 * n - 1) // 2
-	]
+	generators = [ p4 ]
 	actual = get_figurate_numbers(generators)
-	assert actual[0] == (1035, 0)
+	assert actual[0] == (1024, 0)
 
 def test_get_cyclical_figurate_numbers_from_list_simple():
-	figurative_numbers = [(1122, 0), (2211, 1)]
-	actual = get_cyclical_figurate_numbers_from_list(figurative_numbers, 2)
+	figurate_numbers = [(1122, 0), (2211, 1)]
+	actual = get_cyclical_figurate_numbers_from_list(figurate_numbers)
 	assert actual == [1122, 2211]
 
 def test_get_cyclical_figurate_numbers_from_list_same_type():
-	figurative_numbers = [(1122, 0), (2211, 0)]
-	actual = get_cyclical_figurate_numbers_from_list(figurative_numbers, 2)
+	figurate_numbers = [(1122, 0), (2211, 0)]
+	actual = get_cyclical_figurate_numbers_from_list(figurate_numbers)
 	assert actual == None
 
 def test_get_cyclical_figurate_numbers_from_list_too_short_cycle():
-	figurative_numbers = [(1717, 0), (1234, 1)]
-	actual = get_cyclical_figurate_numbers_from_list(figurative_numbers, 2)
+	figurate_numbers = [(1717, 0), (1234, 1)]
+	actual = get_cyclical_figurate_numbers_from_list(figurate_numbers)
 	assert actual == None
 
 def test_get_cyclical_figurate_numbers_from_list_two_lookup_items():
-	figurative_numbers = [(8128, 0), (2809, 1), (8281, 1), (2882, 2)]
-	actual = get_cyclical_figurate_numbers_from_list(figurative_numbers, 3)
+	figurate_numbers = [(8128, 0), (2809, 1), (8281, 1), (2882, 2)] # 2809 and 2882 start both with 28
+	actual = get_cyclical_figurate_numbers_from_list(figurate_numbers)
 	assert actual == [2882, 8281, 8128]
 
 def test__get_cyclical_figurate_numbers_from_list_example():
-	generators = [
-		lambda n : n * (n + 1) // 2,
-		lambda n : n * n,
-		lambda n : n * (3 * n - 1) // 2,
-	]
+	generators = [ p3, p4, p5 ]
 	figurate_numbers = get_figurate_numbers(generators)
-	actual = get_cyclical_figurate_numbers_from_list(figurate_numbers, 3)
+	actual = get_cyclical_figurate_numbers_from_list(figurate_numbers)
 	assert actual == [2882, 8281, 8128]
